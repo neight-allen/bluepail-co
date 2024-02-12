2b6a7a764dc1640853e25457b48c5af3001400ac
neight-allen <neight-allen@users.noreply.github.com>
2024-02-12 04:39:59 +0000

    deploy: ff6968c2c8e175afceaf6175dce6b726b9e9a7c0

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,19 @@
+var store = [{
+        "title": "Beyond the Chatbot: Emerging AI interfaces",
+        "excerpt":"A 15 minute intro for product teams that are interested in incorporating Generative AI like LLMs into their feature roadmaps. Light on technical detail, so it should be a good primer for designers and product managers, or really anyone who has ever managed the development of a web or mobile...","categories": [],
+        "tags": [],
+        "url": "/portfolio/beyond-the-chatbot.html",
+        "teaser": "/assets/images/nate-talking-at-kcai-meetup-aug-08.png"
+      },{
+        "title": "Cables.gl for Stable Diffusion",
+        "excerpt":"The models that power generative AI are a herculean effort by a large number of researchers. But an interface for those models was not really designed in parallel. Now that the audience for these models is broader, we’re obligated to create new intuitive interfaces for these models. Cables.gl is a...","categories": [],
+        "tags": [],
+        "url": "/portfolio/cables-stable-diffusion.html",
+        "teaser": "/assets/images/cables-stable-diffusion-preview.png"
+      },{
+        "title": "Emerging AI interfaces: Beyond the chatbot",
+        "excerpt":"The chatbot demos well. I’m talking, of course, about ChatGPT. We’re not used to having a conversation with our computer. At least not a satisfying one. We’ve become more familiar with awkward phone systems that seem so proud of themselves. How many times a day are you swearing at your...","categories": [],
+        "tags": [],
+        "url": "/2023/08/22/ai-interafaces-beyond-chatbot.html",
+        "teaser": null
+      }]
--- index.md
+++
@@ -1,8 +0,0 @@
----
-# Feel free to add content and custom Front Matter to this file.
-# To modify the layout, see https://jekyllrb.com/docs/themes/#overriding-theme-defaults
-
-layout: home
----
-
-This is a blog and portfolio page for Nate Allen. Email me at [nate@bluepail.co](mailto:nate@bluepail.co)
